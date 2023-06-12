a8260f4b1b4421ea92b3f5875ea5613c28310200
Wesley Vance <me@wesvance.com>
2023-06-11 21:46:33 -0400

    Removed Safe Area Provider and GestureHandlerRootView

--- packages/backpack/src/contexts/Root/RootContext.tsx
+++ packages/backpack/src/contexts/Root/RootContext.tsx
@@ -1,19 +1,16 @@
 import { PageProvider } from '@backpack/contexts/Page'
 import { ThemeProvider } from '@backpack/contexts/Theme'
 import { WindowProvider } from '@backpack/contexts/Window'
-import { GestureHandlerRootView } from '@backpack/utils/reanimated'
-import { SafeAreaProvider } from '@backpack/utils/safeArea'
+
+// import { GestureHandlerRootView } from '@backpack/utils/reanimated'
+// import { SafeAreaProvider } from '@backpack/utils/safeArea'
 
 export const RootContext = ({ children }: { children: JSX.Element }) => {
 	return (
-		<GestureHandlerRootView style={{ flex: 1 }}>
-			<SafeAreaProvider>
-				<WindowProvider>
-					<PageProvider>
-						<ThemeProvider>{children}</ThemeProvider>
-					</PageProvider>
-				</WindowProvider>
-			</SafeAreaProvider>
-		</GestureHandlerRootView>
+		<WindowProvider>
+			<PageProvider>
+				<ThemeProvider>{children}</ThemeProvider>
+			</PageProvider>
+		</WindowProvider>
 	)
 }
