832b46b33c4dd27fd111e72a36de833703576399
Wesley Vance <me@wesvance.com>
2023-06-11 21:56:35 -0400

    Readded safe area provider

--- packages/backpack/src/contexts/Root/RootContext.tsx
+++ packages/backpack/src/contexts/Root/RootContext.tsx
@@ -1,16 +1,16 @@
 import { PageProvider } from '@backpack/contexts/Page'
 import { ThemeProvider } from '@backpack/contexts/Theme'
 import { WindowProvider } from '@backpack/contexts/Window'
-
-// import { GestureHandlerRootView } from '@backpack/utils/reanimated'
-// import { SafeAreaProvider } from '@backpack/utils/safeArea'
+import { SafeAreaProvider } from '@backpack/utils/safeArea'
 
 export const RootContext = ({ children }: { children: JSX.Element }) => {
 	return (
-		<WindowProvider>
-			<PageProvider>
-				<ThemeProvider>{children}</ThemeProvider>
-			</PageProvider>
-		</WindowProvider>
+		<SafeAreaProvider>
+			<WindowProvider>
+				<PageProvider>
+					<ThemeProvider>{children}</ThemeProvider>
+				</PageProvider>
+			</WindowProvider>
+		</SafeAreaProvider>
 	)
 }
