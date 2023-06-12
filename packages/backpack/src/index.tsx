dc8888b3b4e4fc9c8d823e897d3c7d5c6a7e8fdb
Wesley Vance <me@wesvance.com>
2023-06-11 22:22:45 -0400

    Simple test

--- packages/backpack/src/index.tsx
+++ packages/backpack/src/index.tsx
@@ -1,45 +1,45 @@
-export { Avatar } from '@backpack/components/common/Avatar'
-export { Box } from '@backpack/components/common/Box'
-export { Card } from '@backpack/components/common/Card'
-export { Divider } from '@backpack/components/common/Divider'
-export { Image } from '@backpack/components/common/Image'
-export { ImageBackground } from '@backpack/components/common/ImageBackground'
-export { LinearGradient } from '@backpack/components/common/LinearGradient'
-export { Pressable } from '@backpack/components/common/Pressable'
-export { ResponsiveView } from '@backpack/components/common/ResponsiveView'
-export { ScalePress } from '@backpack/components/common/ScalePress'
-export { ScrollView } from '@backpack/components/common/ScrollView'
-export { Skeleton } from '@backpack/components/common/Skeleton'
-export { Stack } from '@backpack/components/common/Stack'
-export { Text } from '@backpack/components/common/Text'
+// export { Avatar } from '@backpack/components/common/Avatar'
+// export { Box } from '@backpack/components/common/Box'
+// export { Card } from '@backpack/components/common/Card'
+// export { Divider } from '@backpack/components/common/Divider'
+// export { Image } from '@backpack/components/common/Image'
+// export { ImageBackground } from '@backpack/components/common/ImageBackground'
+// export { LinearGradient } from '@backpack/components/common/LinearGradient'
+// export { Pressable } from '@backpack/components/common/Pressable'
+// export { ResponsiveView } from '@backpack/components/common/ResponsiveView'
+// export { ScalePress } from '@backpack/components/common/ScalePress'
+// export { ScrollView } from '@backpack/components/common/ScrollView'
+// export { Skeleton } from '@backpack/components/common/Skeleton'
+// export { Stack } from '@backpack/components/common/Stack'
+// export { Text } from '@backpack/components/common/Text'
 
-export { ColorGrid } from '@backpack/components/partials/ColorGrid'
+// export { ColorGrid } from '@backpack/components/partials/ColorGrid'
 
-// Context Hooks & Theme Provider
-export { useTheme } from '@backpack/contexts/Theme'
-export { usePage } from '@backpack/contexts/Page'
-export { useWindow } from '@backpack/contexts/Window'
-export { RootContext } from '@backpack/contexts/Root'
+// // Context Hooks & Theme Provider
+// export { useTheme } from '@backpack/contexts/Theme'
+// export { usePage } from '@backpack/contexts/Page'
+// export { useWindow } from '@backpack/contexts/Window'
+// export { RootContext } from '@backpack/contexts/Root'
 
-// Hooks
-export { useAppForeground } from '@backpack/hooks/utils/useAppForeground'
-export { useDebounce } from '@backpack/hooks/utils/useDebounce'
+// // Hooks
+// export { useAppForeground } from '@backpack/hooks/utils/useAppForeground'
+// export { useDebounce } from '@backpack/hooks/utils/useDebounce'
 
-// Utils
-export * from '@backpack/utils/colors'
-export * from '@backpack/utils/constants'
-export * from '@backpack/utils/getBoxSpacing'
-export * from '@backpack/utils/getResponsiveProp'
-export * from '@backpack/utils/logger'
-export * from '@backpack/utils/numbers'
-export * from '@backpack/utils/random'
-export * from '@backpack/utils/reanimated'
+// // Utils
+// export * from '@backpack/utils/colors'
+// export * from '@backpack/utils/constants'
+// export * from '@backpack/utils/getBoxSpacing'
+// export * from '@backpack/utils/getResponsiveProp'
+// export * from '@backpack/utils/logger'
+// export * from '@backpack/utils/numbers'
+// export * from '@backpack/utils/random'
+// export * from '@backpack/utils/reanimated'
 export {
 	SafeAreaProvider,
 	SafeAreaView,
 	useSafeAreaInsets,
 	EdgeInsets,
 } from '@backpack/utils/safeArea'
-export { styled, ThemeContext, ThemeProvider } from '@backpack/utils/styled'
-export * from '@backpack/utils/styles'
-export * from '@backpack/utils/theme'
+// export { styled, ThemeContext, ThemeProvider } from '@backpack/utils/styled'
+// export * from '@backpack/utils/styles'
+// export * from '@backpack/utils/theme'
